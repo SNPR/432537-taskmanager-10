8f153003b0aaa57aef9aa4c2ae0036f07b7b1056
snpr <vaugk13@gmail.com>
2019-11-22 15:59:29 +0500

    Split code to the components

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import {getBoardCard} from "./board-card";
+import {getBoardList} from "./board-list";
+import {getLoadMoreButton} from "./load-more-button";
+import {getMainFilters} from "./main-filters";
+import {getMenu} from "./menu";
+import {getTaskCreateForm} from "./task-create-form";
+
+export {
+  getBoardCard,
+  getBoardList,
+  getLoadMoreButton,
+  getMainFilters,
+  getMenu,
+  getTaskCreateForm
+};
